00613d936676f71ee2d5ba5fe1ca8e20e0e635b8
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-12 20:52:51 -0300

    get api broken

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
-
+const axios = require('axios');
+const { API_KEY } = process.env
+const {Dogs, Temperament} = require('../db')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,6 +10,35 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+
+
+const apiUrl =  `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`
+
+
+const getApi = async() =>{
+  const data = await axios(apiUrl)
+  console.log(data)
+return data
+}
+
+
+
+
+
+
+//Function get dogs
+
+
+
+
+router.get('/dogs', async (req, res) => {
+  try {
+    res.send(getApi())
+  } catch {
+    console.log('error en get /dogs')
+  }
+})
 
 
 module.exports = router;
