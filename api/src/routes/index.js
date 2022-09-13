62ce53cd28d3b6914984ffef3fceb6d0557c8d21
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-13 17:48:49 -0300

    temperaments ready

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -58,14 +58,14 @@ const getDb = async () => {
       model: Temperament,
       attributes: ['name'], //atributos que quiero traer del modelo Temperament, el id lo trae automatico
       through: {
-          attributes: [],//traer mediante los atributos del modelo
+        attributes: [],//traer mediante los atributos del modelo
       }
     }
   })
 }
 
 //combine data from API and database
-const getAllDogs = async() =>{
+const getAllDogs = async () => {
   const infoApi = await getApi();
   const infoDb = await getDb();
   const infoAll = await infoApi.concat(infoDb)
@@ -88,5 +88,33 @@ router.get('/dogs', async (req, res) => {
   }
 })
 
+
+
+//Ruta get Temperaments
+router.get('/temperament', async (req, res) => {
+
+  try{
+    // Me traigo los Dogs de la api
+    const temperamentApi = await axios(apiUrl)
+  
+    // Guardo en lista de temperamentos todos los resultados despues de aplicarle limpieza a cada uno con split
+    const temperaments = temperamentApi.data.map(e => e.temperament)
+    const temps = temperaments.toString().split(",");
+    console.log(temps)
+  
+    // Encuentro o creo en el modelo de Temperamento, cada temperamento donde el nombre sea igual al dog en el que estoy en ese momento
+    temps.forEach(el => {
+      let i = el.trim()
+      Temperament.findOrCreate({
+        where: { name: i }
+      })
+    })
+  
+    const allTemp = await Temperament.findAll(); // Me traigo todos los temperamentos de la base de datos
+    res.status(200).send(allTemp);
+  }catch{
+    res.status(404).send('error en temperaments')
+  }
+});
 
 module.exports = router;
