ceaa27f5f70e5f670a26abdc2fc6d27a7dbc5229
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-17 16:48:20 -0300

    search bar production

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import style from "../Css/NavBar.module.css";
+import logo from '../Assets/logo.jpg'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className={style.containerDiv}>
+      <div className={style.logoDiv}>
+        <img alt='logo' src={logo} className={style.logo}/>
+        <h1>DOG APP</h1>
+      </div>
+      <div>
+        <h2>SearchBar</h2>
+      </div>
+      <div>
+       <select></select>
+       <select></select>
+       <select></select>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
